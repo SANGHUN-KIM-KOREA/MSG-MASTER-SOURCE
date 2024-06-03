3de646806e919a9ce98eb9a5953631c13b68c8b2
SANGHUN-KIM-KOREA <kimsh153@masterinc.co.kr>
2024-06-03 16:14:58 +0900

    first commit

---
+++ practice.py
@@ -0,0 +1,40 @@
+# 애완동물을 소개해 주세요~
+animal = "강아지"
+name = "연탄이"
+age = 4
+hobby = "산책"
+is_adult = age >= 3
+
+'''이렇게 
+하면 
+여러문장이
+주석처리
+됩니다'''
+
+'''CTRL + /  
+문장을 선택하고 위에 처럼 하면
+주석이 생긴다. 반대로 다시 한번 하면 주석이 지워진다'''
+
+print("우리집 " + animal + "의 이름은 " + name + "이에요")
+print(name + "는 " +str(age)+ "살이며, " + hobby + "을 아주 좋아해요")
+print(name + "는 어른일까요? " + str(is_adult))
+
+station = "사당"
+print(station, "행 열차가 들어오고 있습니다.")
+
+station = "신도림"
+print(station, "행 열차가 들어오고 있습니다.")
+
+station = "인천공항"
+print(station, "행 열차가 들어오고 있습니다.")
+
+print(2**3) # 2^3 = 8
+print(10%3) # 나머지 구하기 1
+print(10//3) # 몹 구하기 3
+print(10 > 3) # True
+print(10 < 3) # False
+print(1 != 3) # True
+print(not(1 != 3)) # False
+
+
+
---
+++ test1_ui.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'test1.ui'
+##
+## Created by: Qt User Interface Compiler version 6.7.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QComboBox, QGroupBox, QLabel,
+    QMainWindow, QMenuBar, QSizePolicy, QStatusBar,
+    QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.groupBox = QGroupBox(self.centralwidget)
+        self.groupBox.setObjectName(u"groupBox")
+        self.groupBox.setGeometry(QRect(150, 240, 120, 80))
+        self.comboBox = QComboBox(self.centralwidget)
+        self.comboBox.setObjectName(u"comboBox")
+        self.comboBox.setGeometry(QRect(200, 140, 68, 22))
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(140, 140, 50, 16))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 800, 33))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"GroupBox", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+    # retranslateUi
+
